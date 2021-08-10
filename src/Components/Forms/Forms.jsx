847158bc30f4902bc7874dc0bfdf7b79259bf340
Vitaliy-1915 <74369854+Vitaliy-1915@users.noreply.github.com>
2021-08-10 16:12:26 +0300

    переписал на хуки

--- src/Components/Forms/Forms.jsx
+++ src/Components/Forms/Forms.jsx
@@ -1,71 +1,139 @@
-import React, { Component } from "react";
+import { useState } from "react";
 import Button from "../Button/Button";
 import { v4 as uuidv4 } from "uuid";
 import { Form, Label, Input } from "./Forms.styles";
 
-class Forms extends Component {
-  state = {
-    name: "",
-    number: "",
-  };
+export default function Forms({ onSubmit }) {
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
 
-  handleChange = (event) => {
+  const handleChange = (event) => {
     const { name, value } = event.currentTarget;
-    this.setState({
-      [name]: value,
-    });
+
+    switch (name) {
+      case "name":
+        setName(value);
+        break;
+
+      case "number":
+        setNumber(value);
+        break;
+
+      default:
+        break;
+    }
   };
 
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
 
-    this.props.onSubmit(this.state);
-    this.reset();
+    onSubmit({ name, number });
+    reset();
   };
 
-  reset = () => {
-    this.setState({
-      name: "",
-      number: "",
-    });
+  const reset = () => {
+    setName("");
+    setNumber("");
   };
 
-  render() {
-    const { name, number } = this.state;
-    return (
-      <Form onSubmit={this.handleSubmit}>
-        <Label>
-          Name
-          <Input
-            id={uuidv4()}
-            type="text"
-            name="name"
-            value={name}
-            onChange={this.handleChange}
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-            required
-          />
-        </Label>
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Label>
+        Name
+        <Input
+          id={uuidv4()}
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleChange}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+          required
+        />
+      </Label>
 
-        <Label>
-          Number
-          <Input
-            id={uuidv4()}
-            type="number"
-            name="number"
-            value={number}
-            onChange={this.handleChange}
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-            required
-          />
-        </Label>
+      <Label>
+        Number
+        <Input
+          id={uuidv4()}
+          type="number"
+          name="number"
+          value={number}
+          onChange={handleChange}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+          required
+        />
+      </Label>
 
-        <Button type="submit" text="Add contact" />
-      </Form>
-    );
-  }
+      <Button type="submit" text="Add contact" />
+    </Form>
+  );
 }
 
-export default Forms;
+// class OldForms extends Component {
+// state = {
+//   name: "",
+//   number: "",
+// };
+
+// handleChange = (event) => {
+//   const { name, value } = event.currentTarget;
+//   this.setState({
+//     [name]: value,
+//   });
+// };
+
+// handleSubmit = (event) => {
+//   event.preventDefault();
+
+//   this.props.onSubmit(this.state);
+//   this.reset();
+// };
+
+// reset = () => {
+//   this.setState({
+//     name: "",
+//     number: "",
+//   });
+// };
+
+//   render() {
+//     const { name, number } = this.state;
+//     return (
+//       <Form onSubmit={this.handleSubmit}>
+//         <Label>
+//           Name
+//           <Input
+//             id={uuidv4()}
+//             type="text"
+//             name="name"
+//             value={name}
+//             onChange={this.handleChange}
+//             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//             title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+//             required
+//           />
+//         </Label>
+
+//         <Label>
+//           Number
+//           <Input
+//             id={uuidv4()}
+//             type="number"
+//             name="number"
+//             value={number}
+//             onChange={this.handleChange}
+//             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//             title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+//             required
+//           />
+//         </Label>
+
+//         <Button type="submit" text="Add contact" />
+//       </Form>
+//     );
+//   }
+// }
+
+// export default Forms;
